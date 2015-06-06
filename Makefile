4f97ddb71c2c251f14be66acb46f829fb1f04952
Justin Carter <bodymindarts@online.de>
2015-06-06 13:15:37 +0200

    get karma running

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 KARMA = ./node_modules/karma/bin/karma
 ESLINT = ./node_modules/eslint/bin/eslint.js
+RUNNER = ./node_modules/mocha/bin/_mocha
 FILES = $(shell find app tests -name "*.js*")
+TESTS = $(shell find tests -name "*Test.js")
 
 install:
 	npm install
@@ -11,3 +13,8 @@ dev:
 lint:
 	node $(ESLINT) $(FILES)
 
+test:
+	$(RUNNER) --ui bdd --reporter dot $(TESTS)
+
+karma:
+	$(KARMA) start
---
+++ karma.conf.js
@@ -0,0 +1,85 @@
+// Karma configuration
+// Generated on Sat Jun 06 2015 13:11:45 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+
+    // list of files / patterns to load in the browser
+    files: [ 'tests/**/*Test.js' ],
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    preprocessors: {
+      'tests/**/*.js*': [ 'webpack' ]
+    },
+
+
+    webpack: {
+      resolve: {
+        extensions: ['', '.js', '.jsx'],
+        modulesDirectories: ['node_modules']
+      },
+      module: {
+        loaders: [
+          { test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel']
+          }
+        ]
+      }
+    },
+
+    plugins: [
+      require('karma-mocha'),
+      require('karma-chai'),
+      require('karma-sinon'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-mocha-reporter'),
+      require('karma-webpack')
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
